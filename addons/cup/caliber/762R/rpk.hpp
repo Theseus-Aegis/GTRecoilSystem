eb3f7a9f9afc10f83fc25eceb7c73f33675ae4e9
TyroneMF <TyroneMF@hotmail.com>
2021-12-22 08:59:29 +0000

    Add 762x39 weapons

---
+++ addons/cup/caliber/762R/rpk.hpp
@@ -0,0 +1,6 @@
+// RPK Rifles inherit
+
+// RPK - Long Barrel
+class CUP_arifle_RPK74: CUP_arifle_AK_Base {
+    recoil = QCLASS(762R_LongBarrel);
+};
--- addons/niarms/CfgWeapons.hpp
+++ addons/niarms/CfgWeapons.hpp
@@ -1,7 +1,12 @@
 class CfgWeapons {
     #include "caliber\baseclasses.hpp"
+
+    // 556
     #include "caliber\556\acr.hpp"
     #include "caliber\556\aug.hpp"
     #include "caliber\556\m4.hpp"
     #include "caliber\556\sig.hpp"
+
+    // 762x39
+    #include "caliber\762R\sig.hpp"
 };
